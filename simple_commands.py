4ba4b2566856ab3ea79763398a4ecd4dc1e19a43
Donnie Bryson <donniebryson@gmail.com>
2024-03-26 13:25:47 -0400

    This is my message

---
+++ simple_commands.py
@@ -0,0 +1,28 @@
+# Simple python commands 
+# The basic math operators in python are: + - / // * %
+# Operators and numbers between the parenthese of a print command execute just as they do in the python 
+# command line. 
+# addition
+print(3 + 3)
+
+# subtraction
+print(6 - 3)
+
+# multiplication. Notice that multiplying by a float triggers the result to be a float
+print(2 * 4)
+print(2 * 4.0)
+
+# Division. Notice that division always returns a float, not an integer
+print(4 / 2)
+
+# You can round down with the // floor division operator. It always rounds down, never up. 
+print(5 / 3)
+print(4 // 3)
+
+# You can get the remainder of division with the modulo operator. 
+print(5 % 3)
+print(5 // 3)
+
+# Raise to a power with **
+# Raise 4 to the power of 2
+print(4**2)
