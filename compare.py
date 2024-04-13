0bcce2bcc50bf15c9b1df18f7cb188908f133ca9
Donnie Bryson <donniebryson@gmail.com>
2024-04-13 18:09:10 -0400

    committing

---
+++ compare.py
@@ -0,0 +1,27 @@
+# Examples of using the comparison operators
+
+import sys
+
+if len(sys.argv) < 4:
+    print("Oops, not enough input")
+    exit()
+
+var1 = sys.argv[1]
+var2 = sys.argv[3]
+comp = sys.argv[2]
+
+if comp == "<":
+    if var1 < var2:
+        print("{0} is less than {1}".format(var1, var2))
+    elif var1 == var2:
+        print("{0} is equal {1}".format(var1, var2))
+    else:
+        print("{0} is not less than {1}".format(var1, var2))
+
+if comp == ">":
+    if var1 > var2:
+        print("{0} is greater than {1}".format(var1, var2))
+    elif var1 == var2:
+        print("{0} is equal {1}".format(var1, var2))
+    else:
+        print("{0} is not greater than {1}".format(var1, var2))
